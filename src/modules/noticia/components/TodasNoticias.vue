9d035a04412ee42e66145842a2916e61287633e1
Domenica Vizcarra <domeviz3410@gmail.com>
2023-09-03 18:01:59 -0500

    Pagina Noticias

--- src/modules/noticia/components/TodasNoticias.vue
+++ src/modules/noticia/components/TodasNoticias.vue
@@ -1,54 +1,72 @@
 <template>
-  <h1>{{ this.totalNoticias }}</h1>
-  <div class="container-noticias">
-    <Noticia v-for="noticia in noticiasPag" v-bind:key="noticia" :noticia="noticia"></Noticia>
+  <main>
+    <h1>Noticias Facultad</h1>
+    <div class="container-noticias">
+      <Noticia v-for="noticia in noticiasPaginadas" :key="noticia.id" :noticia="noticia"></Noticia>
+    </div>
     <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-        <li class="page-item" v-for="(item) in paginas" :key="item"><a class="page-link" href="#" @click="paginar(item)">{{ item }}</a></li>
-        
-        <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+          <a class="page-link" href="#" @click="previousPage">Previous</a>
+        </li>
+        <li class="page-item" v-for="page in paginas" :key="page" :class="{ 'active': currentPage === page }">
+          <a class="page-link" href="#" @click="goToPage(page)">{{ page }}</a>
+        </li>
+        <li class="page-item" :class="{ 'disabled': currentPage === paginas }">
+          <a class="page-link" href="#" @click="nextPage">Next</a>
+        </li>
       </ul>
     </nav>
-  </div>
+  </main>
 </template>
 
 <script>
 import { obtenerTodasNoticiasFachada } from "../helpers/NoticiaCliente.js";
 import Noticia from "./Noticia.vue";
+
 export default {
   components: { Noticia },
   mounted() {
-    this.obtenerTodo();
-   
+    this.obtenerTodasNoticias();
   },
   data() {
     return {
-      noticias: null,
-      paginas:0,
-      noticiasPag:null,
-      totalNoticias:0,
-      noticiasPag:10
+      noticias: [],
+      noticiasPorPagina: 10,
+      totalNoticias: 0,
+      paginas: 0,
+      currentPage: 1,
     };
   },
+  computed: {
+    noticiasPaginadas() {
+      const startIndex = (this.currentPage - 1) * this.noticiasPorPagina;
+      const endIndex = startIndex + this.noticiasPorPagina;
+      return this.noticias.slice(startIndex, endIndex);
+    },
+  },
   methods: {
-    async obtenerTodo() {
+    async obtenerTodasNoticias() {
       const data = await obtenerTodasNoticiasFachada();
       this.noticias = data;
-      this.totalNoticias=this.noticias.length
-      this.paginas=(this.totalNoticias/this.noticiasPag)|0
-      if((this.totalNoticias%this.noticiasPag)>0){
-        this.paginas++
-      }
-      console.log(this.paginas);
-      this.paginar()
+      this.totalNoticias = this.noticias.length;
+      this.paginas = Math.ceil(this.totalNoticias / this.noticiasPorPagina);
     },
-    paginar(index){
-      
-      this.noticiasPag=this.noticias.slice((index-1)*10,((index-1)*10)+9)
-    }
-   
-
+    goToPage(page) {
+      if (page >= 1 && page <= this.paginas) {
+        this.currentPage = page;
+      }
+    },
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.paginas) {
+        this.currentPage++;
+      }
+    },
   },
 };
 </script>
